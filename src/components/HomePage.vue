0a1aad59a4b2d2bafb2d40e0d88d7904fde80177
严俊东 <1172482914@qq.com>
2018-09-17 22:50:59 +0800

    任务列表拆为子路由

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -18,22 +18,7 @@
         <button class="btn btn-default" @click="addList">添加</button>
       </div>
     </div>
-    <div v-if="checkItem">
-      <h2>任务列表[{{checkItem.name}}]</h2>
-      <ul v-if="checkItem.tasks">
-        <li v-for="(item, index) in checkItem.tasks" :key="item._id">
-          <a href="JavaScript:void(0)">
-            <span v-text="item.name"></span>
-          </a>
-          <button @click="deleteTask(item, index)">删除</button>
-          <!-- <button @click="modifyList(item, index)">修改</button> -->
-        </li>
-      </ul>
-      <div>
-        <label>任务名 <input type="text" v-model="newTask.name"></label>
-        <button class="btn btn-default" @click="addTask">添加</button>
-      </div>
-    </div>
+    <!-- 任务列表 -->
     <router-view></router-view>
   </div>
 </template>
@@ -43,9 +28,6 @@ export default {
   data() {
     return {
       newList: {
-        name: ''
-      },
-      newTask: {
         name: ''
       },
       checkItem: null, // 当前显示的清单
@@ -60,12 +42,6 @@ export default {
     this.initData()
   },
   methods: {
-    /** 删除清单 */
-    deleteTask(item, index) {
-      api.deleteTask({}, this.checkItem._id, item._id).then(res => {
-        this.checkItem.tasks.splice(index, 1)
-      })
-    },
     /** 修改清单 */
     modifyList(item, index) {
       let newName = window.prompt('清单名')
@@ -84,22 +60,13 @@ export default {
     checkoutList(item) {
       if (!item.tasks) this.$set(item, 'tasks', [])
       this.checkItem = item
-      api.findTask({}, item._id).then(res => {
-        item.tasks = res.data
-      })
+      this.$router.push({name: 'ListPage', params: {listId: this.checkItem._id}})
     },
     /** 添加清单 */
     addList() {
       api.createList(this.newList).then(res => {
         this.newList.name = ''
         this.listArr.push(res.data)
-      })
-    },
-    /** 添加任务 */
-    addTask() {
-      api.createTask(this.newTask, this.checkItem._id).then(res => {
-        this.newTask.name = ''
-        this.checkItem.tasks.push(res.data)
       })
     },
     /** 初始化数据 */
--- src/components/ListPage.vue
+++ src/components/ListPage.vue
@@ -1,5 +1,64 @@
 <template>
+  <div>
+    <h2>任务列表</h2>
+    <ul v-if="taskArr">
+      <li v-for="(item, index) in taskArr" :key="item._id">
+        <a href="JavaScript:void(0)">
+          <span v-text="item.name"></span>
+        </a>
+        <button @click="deleteTask(item, index)">删除</button>
+        <!-- <button @click="modifyList(item, index)">修改</button> -->
+      </li>
+    </ul>
     <div>
-        123
+      <label>任务名 <input type="text" v-model="newTask.name"></label>
+      <button class="btn btn-default" @click="addTask">添加</button>
     </div>
+  </div>
 </template>
+<script>
+import api from '../api'
+export default {
+  data() {
+    return {
+      listId: null,
+      newTask: {
+        name: ''
+      },
+      taskArr: null
+    }
+  },
+  created() {
+    // 获取链接中的清单ID
+    this.listId = this.$route.params.listId || ''
+    this.initData()
+  },
+  beforeRouteUpdate(to, from, next) {
+    this.listId = to.params.listId
+    this.initData()
+    next()
+  },
+  methods: {
+    /** 数据初始化 */
+    initData() {
+      api.findTask({}, this.listId).then(res => {
+        this.taskArr = res.data
+      })
+    },
+    /** 添加任务 */
+    addTask() {
+      api.createTask(this.newTask, this.listId).then(res => {
+        this.newTask.name = ''
+        this.taskArr.push(res.data)
+      })
+    },
+    /** 删除清单 */
+    deleteTask(item, index) {
+      api.deleteTask({}, this.listId, item._id).then(res => {
+        this.taskArr.splice(index, 1)
+      })
+    },
+  }
+}
+</script>
+
