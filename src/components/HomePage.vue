513fcd5d259778858897f9b141725851d3fdb8ec
严俊东 <1172482914@qq.com>
2019-01-15 10:48:24 +0800

    清单列表使用老接口，暂不载入未完成任务

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -144,7 +144,7 @@ export default {
     },
     /** 获取清单列表 */
     findList() {
-      api.findListAndTask().then(res => {
+      api.findList().then(res => {
         this.listArr.push(...res.data)
         // 根据路由还原指定 清单
         if (this.params.listId) {
